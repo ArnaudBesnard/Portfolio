7f376f21e74cfac115c996fee973898ce7823a04
ArnaudBesnard <besnard.arno@gmail.com>
2019-05-26 10:33:40 +0200

    ajout contact form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -31,5 +32,51 @@ class DefaultController extends Controller
     public function realisationsAction()
     {
         return $this->render('default/realisations.html.twig');
+    }
+    /**
+     * @route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // Create the form according to the FormType created previously.
+        // And give the proper parameters
+        $form = $this->createForm('AppBundle\Form\ContactType', null, array(
+            // To set the action use $this->generateUrl('route_identifier')
+            'action' => $this->generateUrl('contact'),
+            'method' => 'POST'
+        ));
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+                // Send mail
+                if ($this->sendEmail($form->getData())) {
+                    //FLASH MESSAGE FOR REDIRECTION
+                    $request
+                        ->getSession()
+                        ->getFlashBag()
+                        ->add('ajoute', 'Le message a bien été transmis ! Merci');
+
+                    return $this->redirectToRoute('contact');
+                }
+            }
+        }
+        return $this->render('default/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    private function sendEmail($data)
+    {
+        $message = \Swift_Message::newInstance()
+            ->setSubject($data["subject"])
+            ->setFrom($data["email"])
+            ->setReplyTo($data["email"])
+            ->setTo('contact@arnaudbesnard.fr')
+            ->setContentType('text/html')
+            ->setBody($data["message"]);
+
+        if (!$this->get('mailer')->send($message)) {
+            throw new Exception('Le mail n\'a pas pu être envoyé');
+        }
     }
 }
---
+++ src/AppBundle/Form/ContactType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, array('label' => 'Votre nom *'))
+            ->add('subject', TextType::class, array('label' => 'Le sujet de votre message *'))
+            ->add('email', EmailType::class, array(
+                'label' => 'Votre email *',
+            ))
+            ->add('message', TextareaType::class, array(
+                'label' => 'Votre message *',
+                'attr' => array('rows' => 10),
+            ));
+    }
+
+    public function setDefaultOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'error_bubbling' => true
+        ));
+    }
+
+    public function getName()
+    {
+        return 'contact_form';
+    }
+}
